b4ae6769c9c15084815f933c724d36c97463a39e
agayatri <agayatri@agayatri1-sa.stratapps.com>
2025-07-03 22:27:05 +0530

    commit

--- src/test/java/com/stratapps/xamplify/tests/ManageTracksTest.java
+++ src/test/java/com/stratapps/xamplify/tests/ManageTracksTest.java
@@ -21,13 +21,19 @@ public class ManageTracksTest extends BaseTest {
     private static final Logger logger = LogManager.getLogger(ManageTracksTest.class);
     private WebDriverWait wait;
 
-	/*
-	 * @BeforeClass public void setUpClass() { super.setUp(); LoginPage loginPage =
-	 * new LoginPage(driver); loginPage.login(ConfigReader.getProperty("username"),
-	 * ConfigReader.getProperty("password")); manageTracksPage = new
-	 * ManageTracksPage(driver); wait = new WebDriverWait(driver,
-	 * Duration.ofSeconds(60)); logger.info("ManageTracksTest setup completed"); }
-	 */
+	@BeforeClass
+	public void setUpClass() {
+		super.setUp();
+		
+
+        if (!isLoggedIn()) {  // ✅ Only log in if session isn't active
+		LoginPage loginPage = new LoginPage(driver);
+		loginPage.login(ConfigReader.getProperty("username"), ConfigReader.getProperty("password"));
+        }
+		manageTracksPage = new ManageTracksPage(driver);
+		wait = new WebDriverWait(driver, Duration.ofSeconds(60));
+		logger.info("ManageTracksTest setup completed");
+	}
 
     @Test(priority = 1, enabled = true)
     public void navigateToManageTracksTest() {
